c3e9853afb92a551d4c20cbcbc138825e51a7fa9
Marina Krček <marinakrco7@gmail.com>
2018-11-14 19:27:42 +0100

    Day 02, Exercise 02, array
    - add assignment operator
    - add move constructor
    - add copy constructor

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,8 +1,6 @@
 #include "array.h"
 #include <stdexcept> // std::out_of_range
 
-array::array() : array(0, 0) { }
-
 array::array(int size, double value)
 {
 	if (size < 0) return;
@@ -11,6 +9,32 @@ array::array(int size, double value)
 	for (int i = 0; i < n; ++i)
 		p[i] = value;
 }
+
+array::array(const array& other) : n(other.n), p(new double[other.n])
+{
+	std::copy(other.p, other.p + other.n, p);
+}
+
+array::~array()
+{
+	if (!p)
+		return;
+	delete[] p;
+}
+
+array& array::operator=(const array & other)
+{
+	n = other.n;
+	if (p)
+		delete[] p;
+	p = new double[n];
+	std::copy(other.p, other.p + n, p);
+	return *this;
+}
+
+array::array(array&& other) noexcept :
+	n(std::exchange(other.n, 0)), 
+	p(std::exchange(other.p, nullptr)) { }
 
 int array::size()
 {
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -4,8 +4,13 @@ class array
 	double* p;
 	int n;
 public:
-	array();
-	array(int size, double value);
+	array(int size = 0, double value = 0);
+	array(const array& other);
+	~array();
+
+	array& operator=(const array& other);
+
+	array(array&& other) noexcept;
 
 	int size();
 	double at(int index) const;
